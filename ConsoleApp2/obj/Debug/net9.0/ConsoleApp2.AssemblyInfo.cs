33bc86b201e43dab8121ec1bd16d843a60270d89
Devit-rai <Davidrai806@gmail.com>
2025-11-07 08:53:03 +0545

    Changes

--- ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00ae70c8125b8bda37972732ed5c10ecb343806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc258c1b1ac6c3dade69a57ab25f8b7e57ee959a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
